f578e1753d918e24dcbdb99abe62cd52ee9cb338
nickusve <>
2018-07-09 15:59:32 -0400

    Added new function to create an info file if it doesn't exist. Added functions to read and write the current status

--- fileIo.js
+++ fileIo.js
@@ -1,10 +1,26 @@
 const fs = require("fs");
 
-function() {
-	function importCustomerInfo (file)
-	{
+function createNewCustomerInfo(){
+	fs.writeFileSync("information.json", JSON.stringify({
+		orderNumber: "",
+		dealerCode : "",
+		customerLastName :  "",
+		vin : ""
+	}, null, 4));
+}
+
+module.exports = {
+	importCustomerInfo: function(){
+		// Create a new info file if not found
+		if(!fs.existsSync("information.json"))
+		{
+			createNewCustomerInfo();
+		}
+
 		// Read the information file
-		var customerInformation = fs.readFileSync("information.json");
+		var customerInformation = JSON.parse(fs.readFileSync("information.json"));
+
+		customerInformation.freshLoaded = true;
 
 		// If a VIN of correct length is provided use it as the input
 		if(17 === customerInformation.vin.length)
@@ -17,10 +33,19 @@ function() {
 		}
 
 		return customerInformation;
-
+	},
+	importCurrentInfo: function(){
+		// Read current status file if it exists, otherwise return a blank obj
+		if(fs.existsSync("currentStatus.json"))
+		{
+			return JSON.parse(fs.readFileSync("currentStatus.json"));
+		} else
+		{
+			return {};
+		}
+	},
+	exportCurrentInfo: function(data){
+		// Write current status to file
+		fs.writeFileSync("currentStatus.json", JSON.stringify(data, null, 4));
 	}
-
-	export {
-		importCustomerInfo: importCustomerInfo
-	}
-}();
+}
